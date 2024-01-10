97e98994eced85230217cc1d17320dd7c81a1ea3
Shangjin Tang <shangjin.tang@gmail.com>
2024-01-11 02:10:15 +0800

    update 00_hello_world

--- 00_hello_world/main.cpp
+++ 00_hello_world/main.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
 #include <string>
 
-#include "absl/flags/flag.h"
-#include "absl/flags/parse.h"
 #include "fmt/core.h"
 #include "gsl/gsl"
 #include "spdlog/spdlog.h"
 
-// #define ENABLE_DOCTEST_EXAMPLE
-// #define ENABLE_GTEST_EXAMPLE
+// #define ENABLE_MSGSL_EXAMPLES
+// #define ENABLE_ABSL_FLAGS
+// #define ENABLE_DOCTEST
+// #define ENABLE_GTEST
 
-#ifdef ENABLE_DOCTEST_EXAMPLE
+#ifdef ENABLE_DOCTEST
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
-#endif
 
-#ifdef ENABLE_GTEST_EXAMPLE
-#include "gtest/gtest.h"
-#endif
-
-#ifdef ENABLE_DOCTEST_EXAMPLE
 TEST_CASE("Addition") {
     int a = 2;
     int b = 3;
     CHECK(a + b == 5);
 }
 #endif
-#ifdef ENABLE_GTEST_EXAMPLE
+
+#ifdef ENABLE_GTEST
+#include "gtest/gtest.h"
+
 // NOLINTNEXTLINE
 TEST(Subtraction, Integers) {
     int a = 5;
@@ -35,17 +32,30 @@ TEST(Subtraction, Integers) {
 }
 #endif
 
-// ms-gsl
-void processString(gsl::not_null<std::string *> strPtr) {
-    fmt::print("Length of string '{}': {}\n", *strPtr, strPtr->length());
-}
+#ifdef ENABLE_ABSL_FLAGS
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
 
-// abseil: ABSL_FLAG(type, name, default, help-text)
+// abseil flags: ABSL_FLAG(type, name, default, help-text)
 // NOLINTNEXTLINE
 ABSL_FLAG(std::string, powered_by, "abseil-cpp", "Powered-By Information");
+#endif
+
+#ifdef ENABLE_MSGSL_EXAMPLES
+size_t getStringLength(gsl::not_null<std::string *> strPtr) {
+    return strPtr->length();
+}
+#endif
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-#ifdef ENABLE_DOCTEST_EXAMPLE
+    std::cout << "\n----------------------------------------------------------------------\n";
+
+#ifdef ENABLE_ABSL_FLAGS
+    absl::ParseCommandLine(argc, argv);
+    std::cout << "Support parse flags, powered by: " << absl::GetFlag(FLAGS_powered_by) << std::endl;
+#endif
+
+#ifdef ENABLE_DOCTEST
     doctest::Context context;
     int doctest_res = context.run();
     context.applyCommandLine(argc, argv);
@@ -53,26 +63,21 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
         return doctest_res;
 #endif
 
-#ifdef ENABLE_GTEST_EXAMPLE
+#ifdef ENABLE_GTEST
     ::testing::InitGoogleTest(&argc, argv);
     int gtest_res = RUN_ALL_TESTS();
     if (gtest_res)
         return gtest_res;
 #endif
 
-    // spdlog
+#ifdef ENABLE_MSGSL_EXAMPLES
+    std::string str = "ms-gsl";
+    fmt::print("Length of string '{}' is: {}\n", str, getStringLength(&str));
+#endif
+
+    fmt::print("{}, {}!\n", "Hello", "{fmt}");
     spdlog::info("Welcome to spdlog!");
 
-    // fmtlib
-    fmt::print("{}, {}!\n", "Hello", "{fmt}");
-
-    // ms-gsl
-    std::string str = "ms-gsl";
-    processString(&str);
-
-    // abseil
-    absl::ParseCommandLine(argc, argv);
-    std::cout << "Support parse flags, powered by: " << absl::GetFlag(FLAGS_powered_by) << std::endl;
-
+    std::cout << "----------------------------------------------------------------------\n\n";
     return 0;
 }
