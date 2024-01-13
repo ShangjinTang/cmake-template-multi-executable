6a70a5e844d354742f2d1a2dfc664aa25360cdcc
Shangjin Tang <shangjin.tang@gmail.com>
2024-01-13 17:12:15 +0800

    declare linked conan libraries in cmake/depedencies.cmake

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -6,12 +6,30 @@ if(NOT USE_CONAN)
   message(FATAL_ERROR "Please enable conan")
 endif()
 
-# non-test libraries
-find_package(Microsoft.GSL QUIET REQUIRED)
-find_package(absl QUIET REQUIRED)
-find_package(fmt QUIET REQUIRED)
-find_package(spdlog QUIET REQUIRED)
+set(CONAN_LIBRARIES)
 
+# ##############################################################################
+# common libraries
+
+find_package(Microsoft.GSL QUIET REQUIRED)
+list(APPEND CONAN_LIBRARIES Microsoft.GSL::GSL)
+
+find_package(absl QUIET REQUIRED)
+list(APPEND CONAN_LIBRARIES absl::flags absl::flags_parse)
+
+find_package(fmt QUIET REQUIRED)
+list(APPEND CONAN_LIBRARIES fmt)
+
+find_package(spdlog QUIET REQUIRED)
+list(APPEND CONAN_LIBRARIES spdlog::spdlog)
+
+# ##############################################################################
 # test libraries
+
 find_package(GTest QUIET REQUIRED)
+list(APPEND CONAN_LIBRARIES gtest gtest_main)
+
 find_package(doctest QUIET REQUIRED)
+list(APPEND CONAN_LIBRARIES doctest::doctest)
+
+# ##############################################################################
